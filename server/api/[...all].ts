af79605f6b0c45545faa14a54dbbc755e22d89cc
harsh-kakadiya1 <harshkakadiya128@gmail.com>
2026-04-04 21:54:15 +0530

    refactor: Simplify request handling in API and add Vercel configuration

--- server/api/[...all].ts
+++ server/api/[...all].ts
@@ -1,5 +1,4 @@
 import { createApp } from "../index.js";
-import { createServer } from "http";
 
 let cachedAppPromise: ReturnType<typeof createApp> | null = null;
 
@@ -9,20 +8,7 @@ export default async function handler(req: any, res: any) {
   }
 
   const { app } = await cachedAppPromise;
-  
-  // Invoke Express through HTTP server interface
-  return new Promise<void>((resolve) => {
-    const server = createServer(app);
-    server.emit("request", req, res);
-    
-    res.on("finish", () => {
-      resolve();
-    });
-    
-    res.on("close", () => {
-      resolve();
-    });
-    
-    setTimeout(() => resolve(), 30000); // 30s timeout fallback
-  });
+
+  // Express apps are request handlers and can be invoked directly.
+  return app(req, res);
 }
